4455fbd301a4939b056842af2c37dc70ef28cfe3
Tom Porter <tom.x.porter@gmail.com>
2011-11-30 11:59:03 -0500

    Fixing cucumber step definitions for capybara

--- features/jobs_table.feature
+++ features/jobs_table.feature
@@ -2,6 +2,6 @@ Feature:  Jobs Table
 
   Scenario: Reading the Queue Stats page
     When I go to the home page
-    Then I should see a "table" containing "jobs"
+    Then I should see a "tbody" containing at least one "job"
     And I should see the following headings
       | qsid | clientcode | jobid | command | state | user | start_date | start_time | run_time | wait_qsid | thread | queue_name |
--- features/step_definitions/index_steps.rb
+++ features/step_definitions/index_steps.rb
@@ -2,11 +2,11 @@ Then /^There should be a HELP image with tooltip text$/ do
   page.should have_selector('.help img[title]')
 end
 
-Then /^I should see a "([^"]*)" containing "([^"]*)"$/ do |arg1, arg2|
-  #page.should have_selector("#{arg1} .#{arg2}")
-  within "#{arg1}" do |scope|
-    scope.should have_selector(".#{arg2}")
-  end
+Then /^I should see a "([^"]*)" containing at least one "([^"]*)"$/ do |arg1, arg2|
+  page.should have_selector("#{arg1} .#{arg2}")
+  #within "#{arg1}" do |scope|
+  #  scope.should have_selector(".#{arg2}")
+  #end
 end
 
 Then /^I should see the following headings$/ do |table|
