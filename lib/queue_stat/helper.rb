6b638ee2a6970777c2e310b5edd121c9ff20f324
Tom Porter <tom.x.porter@gmail.com>
2011-12-20 14:43:18 -0500

    removed partial DOM load from JS to make work in IE8

--- lib/queue_stat/helper.rb
+++ lib/queue_stat/helper.rb
@@ -50,7 +50,8 @@ module Qstat
       query_hash = get_display_filters
       summary_counts = get_summary_counts
       @jobs = Job.all(query_hash)
-      haml :index, :layout => (request.xhr? ? false : :layout),  :locals => {:active_filters => query_hash.keys, :refresh => session[:refresh], :instance_vars => instance_variable_names, :summary_counts => summary_counts}
+      #haml :index, :layout => (request.xhr? ? false : :layout),  :locals => {:active_filters => query_hash.keys, :refresh => session[:refresh], :instance_vars => instance_variable_names, :summary_counts => summary_counts}
+      haml :index, :locals => {:active_filters => query_hash.keys, :refresh => session[:refresh], :instance_vars => instance_variable_names, :summary_counts => summary_counts}
       #haml :index
     end
 
